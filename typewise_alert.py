88031b203f81c8f1abf231ae34b9ca57a1fe1ff3
Utkarsh Gupta <ututkarshgupta7@gmail.com>
2021-03-31 04:25:07 +0530

    Commit 1
    Initial Addition of Requirements

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,46 +1,57 @@
+class TypewiseAlert:
+    def __init__(self):
+        self.__cooling_types = { 
+                'PASSIVE_COOLING': {'lowerLimit': 0, 'upperLimit': 35},
+                'HI_ACTIVE_COOLING': {'lowerLimit': 0, 'upperLimit': 45},
+                'MED_ACTIVE_COOLING': {'lowerLimit': 0, 'upperLimit': 40}   
+            }
+        
+        self.__alert_message_targets = { 
+                'TO_CONTROLLER': self.send_to_controller,
+                'TO_EMAIL': self.send_to_email
+            }
+        
+        self.__breach_type_email_message = {
+                'TOO_LOW': {
+                    'recepient': "a.b@c.com",
+                    'message': 'Hi, the temperature is too low'
+                    },
+                
+                'TOO_HIGH': {
+                    'recepient': "a.b@c.com",
+                    'message': 'Hi, the temperature is too low'
+                    }
+            }
+        
+        self.__local_controller_header = 0xfeed
+        
+    def get_cooling_type_limits(self, cooling_type):
+        if cooling_type is not None:
+            return self.__cooling_types[cooling_type].values()
 
-def infer_breach(value, lowerLimit, upperLimit):
-  if value < lowerLimit:
-    return 'TOO_LOW'
-  if value > upperLimit:
-    return 'TOO_HIGH'
-  return 'NORMAL'
+    def infer_breach(self, value, lowerLimit, upperLimit):
+        if value < lowerLimit:
+            return 'TOO_LOW'
+        if value > upperLimit:
+            return 'TOO_HIGH'
+        return 'NORMAL'
+        
+    def classify_temperature_breach(self, cooling_type, temperature_in_C):
+        lower_limit, upper_limit = self.get_cooling_type_limits(cooling_type)
+        return self.infer_breach(temperature_in_C, lower_limit, upper_limit)
 
+    def check_and_alert(self, alert_target, battery_char, temperature_in_C):
+        breach_type =\
+        self.classify_temperature_breach(battery_char['cooling_type'], temperature_in_C)
+        if breach_type is not None:
+            self.__alert_message_targets[alert_target](breach_type)
+            return 'SENT'
+        
+    def send_to_controller(self, breach_type):
+        print(f'{self.__local_controller_header}, {breach_type}')
 
-def classify_temperature_breach(coolingType, temperatureInC):
-  lowerLimit = 0
-  upperLimit = 0
-  if coolingType == 'PASSIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 35
-  elif coolingType == 'HI_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 45
-  elif coolingType == 'MED_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 40
-  return infer_breach(temperatureInC, lowerLimit, upperLimit)
+    def send_to_email(self, breach_type):
+        print(f"To: {self.__breach_type_email_message[breach_type]['recepient']}")
+        print('Hi, the temperature is {}'.format(breach_type.replace("_"," ").lower()))
+        
 
-
-def check_and_alert(alertTarget, batteryChar, temperatureInC):
-  breachType =\
-    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
-  if alertTarget == 'TO_CONTROLLER':
-    send_to_controller(breachType)
-  elif alertTarget == 'TO_EMAIL':
-    send_to_email(breachType)
-
-
-def send_to_controller(breachType):
-  header = 0xfeed
-  print(f'{header}, {breachType}')
-
-
-def send_to_email(breachType):
-  recepient = "a.b@c.com"
-  if breachType == 'TOO_LOW':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too low')
-  elif breachType == 'TOO_HIGH':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too high')
--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -1,11 +1,17 @@
 import unittest
-import typewise_alert
-
-
+from typewise_alert import TypewiseAlert
+        
 class TypewiseTest(unittest.TestCase):
-  def test_infers_breach_as_per_limits(self):
-    self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
+    def test_infers_breach_as_per_limits(self):
+        twa = TypewiseAlert()
+        self.assertTrue(twa.infer_breach(20, 50, 100) == 'TOO_LOW')
+        self.assertTrue(twa.infer_breach(200, 50, 100) == 'TOO_HIGH')
+        self.assertTrue(twa.infer_breach(70, 50, 100) == 'NORMAL')
 
+    def test_to_check_and_alert_temperature_status(self):
+        twa = TypewiseAlert()
+        self.assertTrue(twa.check_and_alert('TO_CONTROLLER',{'cooling_type': 'PASSIVE_COOLING'}, 70)=='SENT')
+        self.assertTrue(twa.check_and_alert('TO_EMAIL',{'cooling_type': 'MED_ACTIVE_COOLING'}, 70)=='SENT')
 
 if __name__ == '__main__':
   unittest.main()
