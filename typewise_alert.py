d293a1b00b57ff83282587086f988a27a9b59100
Utkarsh Gupta <ututkarshgupta7@gmail.com>
2021-04-07 10:53:06 +0530

    Commit 3
    1. Addition of console functionality, new validation and coverage, input arguments checks

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,3 +1,5 @@
+from math import isnan
+
 class TypewiseAlert:
     def __init__(self):
         self.__cooling_types = { 
@@ -8,26 +10,34 @@ class TypewiseAlert:
         
         self.__alert_message_targets = { 
                 'TO_CONTROLLER': self.send_to_controller,
-                'TO_EMAIL': self.send_to_email
+                'TO_EMAIL': self.send_to_email,
+                'TO_CONSOLE': self.send_to_console
             }
         
         self.__breach_type_email_message = {
+                'NORMAL': {
+                    'sender': "sender@org.com",
+                    'recepient': "a.b@c.com",
+                    'message': 'Hi, the temperature is in {} state'
+                    },
+                
                 'TOO_LOW': {
+                    'sender': "sender@org.com",
                     'recepient': "a.b@c.com",
                     'message': 'Hi, the temperature is {}'
                     },
                 
                 'TOO_HIGH': {
+                    'sender': "sender@org.com",
                     'recepient': "a.b@c.com",
                     'message': 'Hi, the temperature is {}'
                     }
             }
         
         self.__local_controller_header = 0xfeed
-        
+     
     def get_cooling_type_limits(self, cooling_type):
-        if cooling_type is not None:
-            return self.__cooling_types[cooling_type].values()
+        return self.__cooling_types[cooling_type].values()
 
     def infer_breach(self, value, lowerLimit, upperLimit):
         if value < lowerLimit:
@@ -35,23 +45,49 @@ class TypewiseAlert:
         if value > upperLimit:
             return 'TOO_HIGH'
         return 'NORMAL'
-        
+    
     def classify_temperature_breach(self, cooling_type, temperature_in_C):
-        lower_limit, upper_limit = self.get_cooling_type_limits(cooling_type)
-        return self.infer_breach(temperature_in_C, lower_limit, upper_limit)
-
+        if self.__is_cooling_type_and_temperature_valid(cooling_type, temperature_in_C):
+            lower_limit, upper_limit = self.get_cooling_type_limits(cooling_type)
+            return self.infer_breach(temperature_in_C, lower_limit, upper_limit)
+        else:
+            return 'INVALID_INPUT'
+        
     def check_and_alert(self, alert_target, battery_char, temperature_in_C):
         breach_type =\
-        self.classify_temperature_breach(battery_char['cooling_type'], temperature_in_C)
-        if breach_type is not None:
-            self.__alert_message_targets[alert_target](breach_type)
-            return 'SENT'
+            self.classify_temperature_breach(battery_char['cooling_type'], temperature_in_C)
+        
+        if self.__check_if_alert_target_and_breach_type_are_valid(alert_target, breach_type):
+            return self.__alert_message_targets[alert_target](breach_type)
+        else:
+            return 'INVALID_INPUT'
         
     def send_to_controller(self, breach_type):
-        print(f'{self.__local_controller_header}, {breach_type}')
+        print(f'CONTROLLER MESSAGE: {self.__local_controller_header}, {breach_type}')
+        return "CONTROLLER_INVOKED"
 
     def send_to_email(self, breach_type):
+        print(f"From: {self.__breach_type_email_message[breach_type]['sender']}")
         print(f"To: {self.__breach_type_email_message[breach_type]['recepient']}")
-        print(self.__breach_type_email_message[breach_type]['message'].format(breach_type.replace("_"," ").lower()))
-        
-
+        print(self.__breach_type_email_message[breach_type]['message'].format(breach_type.replace("_"," ")))
+        return 'EMAIL_SENT'
+    
+    def send_to_console(self, breach_type):
+        print(f'CONSOLE MESSAGE: Temperatue is {breach_type.replace("_"," ")}')
+        return 'LOGGED_ON_CONSOLE'
+    
+    def __is_input_temperature_valid(self, temperature_in_C):
+        if temperature_in_C != None and not isnan(temperature_in_C):
+            return True
+        return False
+    
+    def __is_cooling_type_and_temperature_valid(self, cooling_type, temperature_in_C):
+        if self.__is_input_temperature_valid(temperature_in_C) and cooling_type in self.__cooling_types.keys():
+            return True
+        return False
+     
+    def __check_if_alert_target_and_breach_type_are_valid(self, alert_target, breach_type):
+        if alert_target in self.__alert_message_targets.keys() and \
+            breach_type != 'INVALID_INPUT':
+                return True
+        return False   
--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -10,8 +10,14 @@ class TypewiseTest(unittest.TestCase):
 
     def test_to_check_and_alert_temperature_status(self):
         twa = TypewiseAlert()
-        self.assertTrue(twa.check_and_alert('TO_CONTROLLER',{'cooling_type': 'PASSIVE_COOLING'}, 70)=='SENT')
-        self.assertTrue(twa.check_and_alert('TO_EMAIL',{'cooling_type': 'MED_ACTIVE_COOLING'}, 70)=='SENT')
-
+        self.assertTrue(twa.check_and_alert('TO_CONTROLLER', {'cooling_type': 'PASSIVE_COOLING'}, 70)=='CONTROLLER_INVOKED')
+        self.assertTrue(twa.check_and_alert('TO_EMAIL', {'cooling_type': 'MED_ACTIVE_COOLING'}, 70)=='EMAIL_SENT')
+        self.assertTrue(twa.check_and_alert('TO_CONSOLE', {'cooling_type': 'MED_ACTIVE_COOLING'}, 70)=='LOGGED_ON_CONSOLE')
+        self.assertTrue(twa.check_and_alert('TO_EMAIL', {'cooling_type': 'PASSIVE_COOLING'}, float('nan')) == 'INVALID_INPUT')
+        self.assertTrue(twa.check_and_alert('TO_MAIL', {'cooling_type': 'PASSIVE_COOLING'}, 30) == 'INVALID_INPUT')
+        self.assertTrue(twa.check_and_alert('TO_MAIL', {'cooling_type': 'LOW_ACTIVE_COOLING'}, 30) == 'INVALID_INPUT')
+        self.assertTrue(twa.check_and_alert('TO_MAIL', {'cooling_type': 'PASSIVE_COOLING'}, None) == 'INVALID_INPUT')
+        self.assertTrue(twa.check_and_alert('TO_MAIL', {'cooling_type': 'LOW_ACTIVE_COOLING'}, None) == 'INVALID_INPUT')                   
+        
 if __name__ == '__main__':
   unittest.main()
